198c1c8d48b56c8f8098c03b82d0f66ad14031e5
Awspilot <awspilot@Adrians-MacBook.local>
2019-12-24 18:53:20 +0200

    update

--- test/tests/000-new-function-json.js
+++ test/tests/000-new-function-json.js
@@ -1,13 +1,33 @@
 
 describe('lambda deploy new-function', function () {
-	it('log', function(done) {
 
-		exec( 'node', ['./bin/lambda', 'deploy', './test/res/new-function.lambda'] , function( err, data ) {
+
+	it('delete non existing function', function(done) {
+		exec( 'node', ['./bin/lambda', 'delete', './test/res/new-function.lambda'] , function( err, data ) {
 			if (err)
-				throw 'process exit code ' + code.code;
+				throw 'process exit code ' + err.code;
 
 			done()
 		})
-
 	})
+
+	it('create new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'deploy', './test/res/new-function.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+	it('delete new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'delete', './test/res/new-function.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+
 })
