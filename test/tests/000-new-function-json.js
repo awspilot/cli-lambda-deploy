66bd93e9306417139ca392c0a8ab8a5dfb81dbe9
Awspilot <awspilot@Adrians-MacBook.local>
2019-12-24 18:14:20 +0200

    update

--- test/tests/000-new-function-json.js
+++ test/tests/000-new-function-json.js
@@ -3,20 +3,12 @@ describe('lambda deploy new-function', function () {
 	it('log', function(done) {
 		//console.log(process.env)
 
-		var spawn = require('child_process').spawn;
-		var prc = spawn('node',  ['./bin/lambda', 'deploy', './test/res/new-function.lambda']);
+		exec( 'node', ['./bin/lambda', 'deploy', './test/res/new-function.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + code.code;
 
-		prc.stdout.setEncoding('utf8');
-		prc.stdout.on('data', function (data) {
-			console.log(data.toString())
-		});
-		prc.stderr.on('data', function (data) {
-			console.log(data.toString())
-		});
-		prc.on('close', function (code) {
-			if (code !== 0 )
-				throw 'process exit code ' + code;
 			done()
-		});
+		})
+
 	})
 })
