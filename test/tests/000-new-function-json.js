e8f5feed2d62124cb77e8a9f4958a12ec6829fff
adrianpraja <adrian@databank.ro>
2019-12-24 11:48:57 +0200

    update

--- test/tests/000-new-function-json.js
+++ test/tests/000-new-function-json.js
@@ -1,7 +1,22 @@
 
 describe('lambda deploy new-function', function () {
 	it('log', function(done) {
-		console.log(process.env)
-		done()
+		//console.log(process.env)
+
+		var spawn = require('child_process').spawn;
+		var prc = spawn('node',  ['./bin/lambda', 'deploy', './test/res/new-function.lambda']);
+
+		prc.stdout.setEncoding('utf8');
+		prc.stdout.on('data', function (data) {
+			console.log(data.toString())
+		});
+		prc.stderr.on('data', function (data) {
+			console.log(data.toString())
+		});
+		prc.on('close', function (code) {
+			if (code !== 0 )
+				throw 'process exit code ' + code;
+			done()
+		});
 	})
 })
