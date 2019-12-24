80fd1bf06fc0c9f4971df855fb09433027dcdc07
Awspilot <awspilot@Adrians-MacBook.local>
2019-12-24 18:14:05 +0200

    update

--- test/000-index.js
+++ test/000-index.js
@@ -1,2 +1,21 @@
 
+exec = function( cmd, params, cb ) {
+	var spawn = require('child_process').spawn;
+	var prc = spawn( cmd,  params );
+
+	prc.stdout.setEncoding('utf8');
+	prc.stdout.on('data', function (data) {
+		console.log(data.toString())
+	});
+	prc.stderr.on('data', function (data) {
+		console.log(data.toString())
+	});
+	prc.on('close', function (code) {
+		if (code !== 0 )
+			return cb({ code: code })
+
+		cb()
+	});
+}
+
 require("./tests/000-new-function-json")
