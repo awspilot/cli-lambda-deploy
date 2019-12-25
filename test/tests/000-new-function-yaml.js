d0cba578e856fec8123b2362d3f451b9adcc0790
Awspilot <awspilot@Adrians-MacBook.local>
2019-12-25 11:15:45 +0200

    yaml tests

---
+++ test/tests/000-new-function-yaml.js
@@ -0,0 +1,51 @@
+
+describe('lambda deploy new-function', function () {
+
+
+	it('delete non existing function', function(done) {
+		exec( 'node', ['./bin/lambda', 'delete', './test/res/new-function-yaml-' + process.version.split('.')[0] + '.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+	it('create new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'deploy', './test/res/new-function-yaml-' + process.version.split('.')[0] + '.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+	it('create(existing) new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'deploy', './test/res/new-function-yaml-' + process.version.split('.')[0] + '.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+	it('invoke new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'invoke', './test/res/new-function-yaml-' + process.version.split('.')[0] + '.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+	it('delete new-function', function(done) {
+		exec( 'node', ['./bin/lambda', 'delete', './test/res/new-function-yaml-' + process.version.split('.')[0] + '.lambda'] , function( err, data ) {
+			if (err)
+				throw 'process exit code ' + err.code;
+
+			done()
+		})
+	})
+
+
+})
