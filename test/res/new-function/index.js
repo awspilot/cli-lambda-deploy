4961334899b6bcaaa5de1d1930425c7d4bcf930e
Awspilot <awspilot@Adrians-MacBook.local>
2019-12-26 10:34:04 +0200

    remove keys from environment

--- test/res/new-function/index.js
+++ test/res/new-function/index.js
@@ -1,8 +1,13 @@
 exports.handler = function( event, context, cb ) {
+	var clean_env = JSON.parse(JSON.stringify(process.env))
+	delete clean_env.AWS_ACCESS_KEY_ID;
+	delete clean_env.AWS_SECRET_ACCESS_KEY;
+	delete clean_env.AWS_SESSION_TOKEN;
+
 	var out = {
 		version: process.version,
 		event: event,
-		environment: process.env,
+		environment: clean_env,
 	}
 
 	console.log(JSON.stringify(out, null, "\t"))
